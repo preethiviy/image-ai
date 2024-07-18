e43110ecc3c50cfa750f02dc86b679d7dbea098f
preethiviy <preethivirajyambem@gmail.com>
2024-07-18 20:55:14 +0530

    editor layout setup

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -3,6 +3,10 @@
 import { useEffect, useRef } from "react";
 import { fabric } from "fabric";
 import { useEditor } from "../hooks/use-editor";
+import { Navbar } from "./navbar";
+import { Sidebar } from "./sidebar";
+import { Toolbar } from "./toolbar";
+import { Footer } from "./footer";
 
 export const Editor = () => {
     const {init} = useEditor();
@@ -26,8 +30,16 @@ export const Editor = () => {
 
     return (
         <div className="h-full flex flex-col">
-            <div ref={containerRef} className="flex-1 h-full bg-muted">
-                <canvas ref={canvasRef} />
+            <Navbar />
+            <div className="absolute h-[calc(100%-68px)] w-full top-[68px] flex">
+                <Sidebar />
+                <main className="bg-muted flex-1 overflow-auto relative flex flex-col">
+                    <Toolbar />
+                    <div ref={containerRef} className="flex-1 h-[calc(100%-124px)] bg-muted">
+                        <canvas ref={canvasRef} />
+                    </div>
+                    <Footer />
+                </main>
             </div>
         </div>
     )
