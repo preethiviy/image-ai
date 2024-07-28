45c61e0944abe3a1e750cffab6483e75b2659ac8
preethiviy <preethivirajyambem@gmail.com>
2024-07-29 01:56:25 +0530

    unspash api, image sidebar

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,12 +1,12 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-import user from "./user";
+import images from "./images";
 
 export const runtime = "nodejs";
 
 const app = new Hono().basePath("/api");
 
-const routes = app.route("/user", user);
+const routes = app.route("/images", images);
 
 export const GET = handle(app);
 
