dd18d8403cc08c39c5701638875256b1dedc873e
preethiviy <preethivirajyambem@gmail.com>
2024-08-08 10:34:34 +0530

    verify auth backend

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,12 +1,23 @@
-import { Hono } from "hono";
+import { Hono, Context } from "hono";
 import { handle } from "hono/vercel";
 import images from "./images";
 import users from "./users";
 import ai from "./ai";
+import { AuthConfig, initAuthConfig } from "@hono/auth-js";
+import authConfig from "@/auth.config";
 
 export const runtime = "nodejs";
 
+function getAuthConfig(c: Context): AuthConfig {
+    return {
+        secret: c.env.AUTH_SECRET,
+        ...authConfig
+    }
+}
+
 const app = new Hono().basePath("/api");
+
+app.use("*", initAuthConfig(getAuthConfig));
 
 const routes = app
     .route("/ai", ai)
